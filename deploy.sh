5e3769a42bbe4e357f3ac84669316fc0e88f0b89
ubergeek77 <ubergeek77@users.noreply.github.com>
2024-06-19 15:48:24 -0500

    Version bump

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LED_CURRENT_VERSION="1.3.4"
+LED_CURRENT_VERSION="1.4.0"
 
 # cd to the directory the script is in
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
